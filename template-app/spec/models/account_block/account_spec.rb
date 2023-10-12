8be6f6e587c97eed41471c193eb7a98eeae32023
Mit Donga <meetdonga5344@gmail.com>
2023-10-12 12:13:25 +0000

    [1696922] Fixed New Security Hotspots sonarqube issue

--- template-app/spec/models/account_block/account_spec.rb
+++ template-app/spec/models/account_block/account_spec.rb
@@ -4,14 +4,13 @@ RSpec.describe AccountBlock::Account, type: :model do
     describe 'check_email_validation' do
         before(:each) do
             @company = create(:company)
-            @password = 'Test@1234'
         end
         it "should generate password" do
             user = AccountBlock::Account.new(
                 first_name: "Rahul",
                 last_name: "Patel",
                 email: Faker::Internet.email,
-                password: @password,
+                password: 'Test@1234',
                 country_code: "91",
                 account_type: "venue",
                 phone_number: Faker::Base.numerify('89########'),
@@ -20,7 +19,7 @@ RSpec.describe AccountBlock::Account, type: :model do
             user.save!
             password = user.generate_password
             assert_equal user.authenticate(password), false
-            assert_equal user.authenticate(@password), user
+            assert_equal user.authenticate('Test@1234'), user
         end
     end
 end
