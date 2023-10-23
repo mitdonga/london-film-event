929b1f22d269cfcd581b41cd4a1db2fa5c5ac82a
Mit Donga <meetdonga5344@gmail.com>
2023-10-23 04:12:31 +0000

    [1650196] Fixed build error due to active admin database hit during load

--- template-app/config/routes.rb
+++ template-app/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   get "/healthcheck", to: proc { [200, {}, ["Ok"]] }
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-
+  ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
   namespace :bx_block_login do
     post "login", to: "logins#create"
   end
