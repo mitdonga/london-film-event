df7a00c87c5071208b82ee83a54ea1a955340a54
Mit Donga <meetdonga5344@gmail.com>
2023-10-25 08:33:02 +0000

    Fixing code smells

--- template-app/app/assets/javascripts/active_admin.js
+++ template-app/app/assets/javascripts/active_admin.js
@@ -1,16 +1,3 @@
 //= require arctic_admin/base
 //= require activeadmin/quill_editor/quill
 //= require activeadmin/quill_editor_input
-
-// $(document).ready(function() {
-//     let l1 = $('a[href="/admin/lf_admins/new"]')
-//     if (l1.length && l1.text().includes("Lf Admin")) {
-//         l1.text("New LF Admin");
-//     }
-
-//     let l2 = $('a[href="/admin/lf_admins"]');
-//     if (l2.length && l2.text() === "Lf Admins") {
-//         l2.text("LF Admins");
-//     }
-
-// });
