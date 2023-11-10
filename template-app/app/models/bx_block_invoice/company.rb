e79a2a9a34ed0c41c3e702b4a50cea89ca52fba8
Mit Donga <meetdonga5344@gmail.com>
2023-11-10 06:40:33 +0000

    [1730994] service management done in admin console

--- template-app/app/models/bx_block_invoice/company.rb
+++ template-app/app/models/bx_block_invoice/company.rb
@@ -22,17 +22,24 @@ module BxBlockInvoice
       data.group_by {|sc| sc["service"]}
     end
 
+    def services_sub_categories_data
+      services = company_categories.includes(:category).order(:id).map {|cc| {:company_category_id => cc.id, :service_id => cc.category.id, :service_name => cc.category.name, :has_access => cc.has_access}}
+      services.map do |s|
+        data = company_sub_categories.joins(:sub_category => :parent).where("categories.id = ?", s[:service_id])
+        filtered_data = data.map {|s| {:company_sub_category_id => s.id, :price => s.price, :sub_category_name => s.sub_category.name}}
+        s.merge({company_sub_categories: filtered_data})
+      end
+    end
+
     private
 
     def add_company_sub_categories
-      puts "========== Inside add_company_sub_categories ==========="
       sub_categories = BxBlockCategories::SubCategory.select(:id, :start_from)
       data = sub_categories.map {|sc| { company_id: self.id, sub_category_id: sc[:id], price: sc[:start_from] }}
       BxBlockInvoice::CompanySubCategory.create!(data)
     end
 
     def add_company_categories
-      puts "========== Inside add_company_categories ==========="
       service_ids = BxBlockCategories::Service.ids
       data = service_ids.map {|i| { company_id: self.id, category_id: i}}
       BxBlockInvoice::CompanyCategory.create!(data)
