55677be9e8bdf67bf25a6d365abe9aca51be5921
shvyas@bestpeers.com <shvyas@bestpeers.com>
2023-12-18 11:05:25 +0000

    [1881766] fixed code smell

--- template-app/spec/factories/account_block/account.rb
+++ template-app/spec/factories/account_block/account.rb
@@ -37,7 +37,7 @@ FactoryBot.define do
         country_code { "+91" }
         phone_number { Faker::Base.numerify('81########') }
         account_type { "venue" }
-        full_phone_number {Faker::Base.numerify('+918#########')}
+        full_phone_number {Faker::Base.numerify('+919#########')}
         activated {true}
         company_id { FactoryBot.create(:company).id }
         client_admin_id { FactoryBot.create(:client_admin).id }
