9fbc652ed1b850985bfe2b17b20fdf94b0b29766
shvyas@bestpeers.com <shvyas@bestpeers.com>
2023-12-20 08:01:41 +0000

    [1881770] fixed email credentials and views

--- template-app/app/mailers/bx_block_contact_us/contact_mailer.rb
+++ template-app/app/mailers/bx_block_contact_us/contact_mailer.rb
@@ -2,7 +2,7 @@ module BxBlockContactUs
   class ContactMailer < ApplicationMailer
     def send_mail(user)
       @user = user
-      mail(to: 'brotaukiweuppoi-8051@yopmail.com', subject: "Welcome to Our Site #{@user.first_name}")
+      mail(to: 'admin01@yopmail.com',from: @user.email, subject: "Welcome to Our Site #{@user.first_name}")
     end 
   end
 end
