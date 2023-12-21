2aa83ec68a30f64ddaf647897dd5205c0acb0e12
Mit Donga <meetdonga5344@gmail.com>
2023-12-21 14:56:32 +0000

    [1791287] Added validations in company input field

--- template-app/app/models/bx_block_categories/company_input_field.rb
+++ template-app/app/models/bx_block_categories/company_input_field.rb
@@ -2,12 +2,31 @@ module BxBlockCategories
     class CompanyInputField < BxBlockCategories::ApplicationRecord
         self.table_name = :company_input_fields
 
+        before_validation :sanitize_columns
+
         validates :company_id, uniqueness: { scope: :input_field_id, message: "Input value already attached with this company"}
+        validate :validate_edge_case
 
         belongs_to :company, class_name: "BxBlockInvoice::Company"
         belongs_to :input_field, class_name: "BxBlockCategories::InputField"
 
         private
+
+        def validate_edge_case
+            if input_field.values.present? && inputs_size(input_field.values) != inputs_size(self.values)
+                errors.add(:values, "Invalid, should provide #{inputs_size(input_field.values)} values")
+            elsif input_field.multiplier.present? && inputs_size(input_field.values) != inputs_size(self.values)
+                errors.add(:multiplier, "Invalid, should provide #{inputs_size(input_field.multiplier)} multipliers")
+            elsif input_field.default_value.present? && !self.default_value.present?
+                errors.add(:default_value, "Must present")
+            elsif input_field.values.blank? && self.values.present?
+                errors.add(:values, "Must blank")
+            elsif input_field.multiplier.blank? && self.multiplier.present?
+                errors.add(:multiplier, "Must blank")
+            elsif input_field.default_value.blank? && self.default_value.present?
+                errors.add(:default_value, "Must blank")
+            end
+        end
 
         def sanitize_columns
             self.values = sanitize_string(self.values) if self.values.present?
@@ -17,6 +36,11 @@ module BxBlockCategories
         def sanitize_string(str)
             return unless str.present?
             str.to_s.split(",").select {|e| e.present?}.map {|e| e.strip}.join(", ")
+        end
+
+        def inputs_size(str)
+            return unless str.present?
+            str.to_s.split(",").size
         end
     end
 end
