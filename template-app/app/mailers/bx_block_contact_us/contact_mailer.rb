aaf03baca4f282d491ab348afe7546f433e12ffb
shvyas@bestpeers.com <shvyas@bestpeers.com>
2024-01-08 06:55:35 +0000

    [1898804] fixed rspecs

--- template-app/app/mailers/bx_block_contact_us/contact_mailer.rb
+++ template-app/app/mailers/bx_block_contact_us/contact_mailer.rb
@@ -7,7 +7,7 @@ module BxBlockContactUs
 
       mail(to: admin_emails.map(&:email),
       from: @user.email, 
-      subject: "Welcome to Our Site #{@user.first_name}")
+      subject: "Welcome to Our Site #{@user.first_name}") if admin_emails.any?
     end
 
     def send_profile_mail(user)
@@ -19,12 +19,12 @@ module BxBlockContactUs
         lf_and_client_admin_mails = (admins_mail + client_admins).uniq
         mail(to: lf_and_client_admin_mails.map(&:email),
         from: @profile_user.email, 
-        subject: "Welcome to Our Site #{@profile_user.first_name}")
+        subject: "Welcome to Our Site #{@profile_user.first_name}") if lf_and_client_admin_mails.any?
       else
         clients_admins = AdminUser.all
         mail(to: clients_admins.map(&:email),
         from: @profile_user.email, 
-        subject: "Welcome to Our Site #{@profile_user.first_name}")
+        subject: "Welcome to Our Site #{@profile_user.first_name}") if clients_admins.any?
       end
     end 
   end
