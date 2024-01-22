13fa547cd0a38d428b0ff0df1a4b17adda53e98c
Mit Donga <meetdonga5344@gmail.com>
2024-01-22 14:59:54 +0000

    [1650265] Fixed test cases

--- template-app/spec/factories/email_template.rb
+++ template-app/spec/factories/email_template.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
     factory :email_template, class: "BxBlockEmailNotifications::EmailTemplate" do
       name { Faker::Lorem.sentence(word_count: 4) }
-      body { Faker::Lorem.paragraph_by_chars }
+      body { "<h4>Hi {user_name}</h4> <br> <p>" + Faker::Lorem.paragraph_by_chars + "</p>" }
+      dynamic_words { "user_name"}
     end
   end
   
