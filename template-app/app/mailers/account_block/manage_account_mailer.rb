fd772032aa709735fe982c9fbb88fceea434fecd
Mit Donga <meetdonga5344@gmail.com>
2024-01-26 09:48:27 +0000

    [1650292] Removed water mark from email template

--- template-app/app/mailers/account_block/manage_account_mailer.rb
+++ template-app/app/mailers/account_block/manage_account_mailer.rb
@@ -1,11 +1,13 @@
 module AccountBlock
     class ManageAccountMailer < ApplicationMailer
+      WATER_MARK_HTML = '<p data-f-id="pbf" style="text-align: center; font-size: 14px; margin-top: 30px; opacity: 0.65; font-family: sans-serif;">Powered by <a href="https://www.froala.com/wysiwyg-editor?pb=1" title="Froala Editor">Froala Editor</a></p>'
+
         def send_welcome_mail_to_user(user_id)
             account = Account.find user_id
 
             template = BxBlockEmailNotifications::EmailTemplate.find_by_name("User Account Creation (Mail To User)")
             return unless template.present? && account.present?
-            email_body = template.body.gsub('{first_name}', account.first_name).gsub('{last_name}', account.last_name).gsub('{email}', account.email).gsub('{user_name}', account.full_name)
+            email_body = template.body.gsub('{first_name}', account.first_name).gsub('{last_name}', account.last_name).gsub('{email}', account.email).gsub('{user_name}', account.full_name).gsub(WATER_MARK_HTML, '')
 
             to_emails = account.email
 
@@ -23,7 +25,7 @@ module AccountBlock
 
             template = BxBlockEmailNotifications::EmailTemplate.find_by_name("User Account Creation (Mail To LF Admin)")
             return unless template.present? && account.present?
-            email_body = template.body.gsub('{first_name}', account.first_name).gsub('{last_name}', account.last_name).gsub('{email}', account.email).gsub('{full_phone_number}', account.full_phone_number)
+            email_body = template.body.gsub('{first_name}', account.first_name).gsub('{last_name}', account.last_name).gsub('{email}', account.email).gsub('{full_phone_number}', account.full_phone_number).gsub(WATER_MARK_HTML, '')
 
             to_emails = AdminUser.all.pluck(:email)
 
