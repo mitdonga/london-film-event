3e9c0caacc520359329e3e2d9e2938dfa1afffb4
Mit Donga <meetdonga5344@gmail.com>
2024-02-06 09:13:11 +0000

    [1996408] Tried to fix xero ruby error

--- template-app/app/services/account_block/xero_api_service.rb
+++ template-app/app/services/account_block/xero_api_service.rb
@@ -1,13 +1,11 @@
-require 'xero-ruby'
-
 module AccountBlock
   class XeroApiService
     CREDENTIALS = Rails.application.config.xero_credentials
     XERO_TENANT_ID = Rails.application.config.xero_tenant_id
-    @@xero_client = XeroRuby::ApiClient.new(credentials: CREDENTIALS)
-
+    
     def initialize
-      @@xero_client.get_client_credentials_token 
+      @xero_client = XeroRuby::ApiClient.new(credentials: CREDENTIALS)
+      @xero_client.get_client_credentials_token 
     end
 
     def get_invoices(user, inv_status=nil,  page=1)
@@ -29,15 +27,15 @@ module AccountBlock
         }
       }
 
-      @invoices = user.xero_id.present? && Rails.env != "test" ? @@xero_client.accounting_api.get_invoices(XERO_TENANT_ID, opts).invoices : []
+      @invoices = user.xero_id.present? && Rails.env != "test" ? @xero_client.accounting_api.get_invoices(XERO_TENANT_ID, opts).invoices : []
     end
 
     def invoice_pdf(invoice_id)
-      @@xero_client.accounting_api.get_invoice_as_pdf(XERO_TENANT_ID, invoice_id)
+      @xero_client.accounting_api.get_invoice_as_pdf(XERO_TENANT_ID, invoice_id)
     end
 
     def contacts
-      @contacts = @@xero_client.accounting_api.get_contacts('').contacts
+      @contacts = @xero_client.accounting_api.get_contacts('').contacts
     end
 
     def create_contact(user)
@@ -59,7 +57,7 @@ module AccountBlock
         contacts: [contact]
       }
       return if Rails.env == "test"
-      response = @@xero_client.accounting_api.create_contacts(XERO_TENANT_ID, contacts)
+      response = @xero_client.accounting_api.create_contacts(XERO_TENANT_ID, contacts)
       user.update(xero_id: response.contacts.first.contact_id)
     end
   end
