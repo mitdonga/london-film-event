0c17fdc7094e29ef90cbb2190cd56bb55bba6c13
Mit Donga <meetdonga5344@gmail.com>
2024-02-13 04:05:19 +0000

    [2008713] Added test cases for inquiries

--- template-app/spec/admin/inquiries_spec.rb
+++ template-app/spec/admin/inquiries_spec.rb
@@ -4,49 +4,35 @@ require 'factory_bot'
 
 include Warden::Test::Helpers
 
-RSpec.describe Admin::ServicesController, type: :controller do
+RSpec.describe Admin::InquiriesController, type: :controller do
+  include_context "setup data"
   render_views
   before(:each) do
     @admin = AdminUser.create!(email: 'test123@example.com', password: 'password', password_confirmation: 'password')
     @admin.save
-    @service = FactoryBot.create(:service)
-    3.times do
-      FactoryBot.create(:input_field, inputable: @service)
-    end
+    # @service = FactoryBot.create(:service)
+    # 3.times do
+    #   FactoryBot.create(:input_field, inputable: @service)
+    # end
 
     sign_in @admin
   end
-  describe "Service#new" do
-    let(:params) do {
-        name: Faker::Lorem.sentence(word_count: 2),
-        description: Faker::Lorem.paragraph(sentence_count: 200)
-      }
-    end
-    it "create service" do
-      post :new, params: params
-      expect(response).to have_http_status(200)
-    end
-  end
-  describe "Get#index" do
+  describe "Get#inquiry" do
     it "show all services" do
       get :index
       expect(response).to have_http_status(200)
     end
   end
   describe "Get#show" do
-    it "show service" do
-      get :show, params: {id: @service.id}
+    it "show inquiry" do
+      get :show, params: {id: @inquiry_1.id}
       expect(response).to have_http_status(200)
     end
   end
   describe "Put#edit" do
-    let(:params) do {
-      name: Faker::Lorem.sentence(word_count: 2)
-    }
-    end
-    it "edit service" do
-        put :update, params: {id: @service.id, account: params}
-        expect(response).to have_http_status(302)
+    it "edit inquiry" do
+      put :update, params: {id: @inquiry_1.id, inquiry: {status: "approved"}}
+      expect(response).to have_http_status(302)
     end
   end
 end
