657d98dea987609a23029cdb0d969a5740e3c573
Mit Donga <meetdonga5344@gmail.com>
2024-03-12 13:15:27 +0000

    [1791262] Fixed service name validation error in unit test cases

--- template-app/spec/factories/service.rb
+++ template-app/spec/factories/service.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
     factory :service, class: "BxBlockCategories::Service" do
-        name { Faker::Lorem.sentence(word_count: 2) }
+        name { Faker::Lorem.sentence(word_count: 5) }
         description { Faker::Lorem.paragraph(sentence_count: 200) }
     end
 end
