db355fb77addae9814516466689ee82bd1e14da9
Mit Donga <meetdonga5344@gmail.com>
2024-04-23 09:09:42 +0000

    [2201743] Form for Create bespoke service for company and AJAX javascript done

--- template-app/app/assets/javascripts/active_admin.js
+++ template-app/app/assets/javascripts/active_admin.js
@@ -1,60 +1,34 @@
 //= require arctic_admin/base
-
 //= require activeadmin_addons/all
-
 
 $(document).ready(function(){function a(a){var c=$("#client_admin_select"),n=$("#client_admin_select").val();c.empty(),$.each(a,function(a,n){c.append($("<option>").text(n.name).attr("value",n.id))}),null!=a.find(a=>a.id==n)?c.val(n):c.prop("selectedIndex",-1)}var c=$("#company_select").val();c?$.ajax({url:"/account_block/accounts/all_company_users",data:{company_id:c},dataType:"json",success:function(c){a(c.data)}}):$("#client_admin_select").empty(),$("#company_select").change(function(){var c=$(this).val();c?$.ajax({url:"/account_block/accounts/all_company_users",data:{company_id:c},dataType:"json",success:function(c){a(c.data)}}):$("#client_admin_select").empty()})});
 
 $(document).ready(function() {
-  // Function to open the popup
-  function openPopup() {
-    document.getElementById('modalDialog').style.display = 'block';
-  }
-  // Function to close the popup
-  function closePopup() {
-    document.getElementById('modalDialog').style.display = 'none';
-  }
+  $('#create_bespoke_service_from').on('submit', function(event) {
+    event.preventDefault();
+    var formData = $(this).serialize();
+    $.ajax({
+      url: $(this).attr('action'),
+      method: 'POST',
+      data: formData,
+      dataType: 'json', 
+      success: function(response) {
+        var res = JSON.parse(response)
+        console.log(res);
+        $(".model-error").html("")
+      },
+      error: function(xhr, status, error) {
+        var res = JSON.parse(xhr.responseText)
+        $(".model-error").html(res.error)
+      }
+    });
+  })
 
-  // Function to handle form submission
-  // function submitForm(formData) {
-  //   fetch('/your_controller/action', {
-  //     method: 'POST',
-  //     body: formData
-  //   })
-  //   .then(response => {
-  //     // Handle response as needed
-  //     if (response.ok) {
-  //       closePopup();
-  //       // Optionally, update UI or show success message
-  //     } else {
-  //       // Handle error response
-  //     }
-  //   })
-  //   .catch(error => {
-  //     // Handle fetch error
-  //   });
-  // }
+  $('#create_bespoke_service_btn').on('click', function() {
+    $('#modalDialog').show();
+  })
 
-  // Bind click event to open the popup when the button is clicked
-  document.querySelector('#create_bespoke_service_btn').addEventListener('click', function() {
-    openPopup();
-  });
-
-  document.querySelector('#close_btn').addEventListener('click', function() {
-    closePopup();
-  });
-
-  // // Bind form submission event
-  // document.getElementById('popup-form').addEventListener('submit', function(event) {
-  //   event.preventDefault(); // Prevent default form submission
-  //   const formData = new FormData(event.target); // Get form data
-  //   submitForm(formData); // Call function to submit form data
-  // });
-
-  // // Bind click event to close the popup when clicking outside the popup or on a close button
-  // document.addEventListener('click', function(event) {
-  //   if (!event.target.closest('#popup-container') && !event.target.closest('.open-popup-button')) {
-  //     closePopup();
-  //   }
-  // });
+  $('#close_btn').on('click', function() {
+    $('#modalDialog').hide();
+  })
 });
--- template-app/app/controllers/bx_block_categories/categories_controller.rb
+++ template-app/app/controllers/bx_block_categories/categories_controller.rb
@@ -131,23 +131,25 @@ module BxBlockCategories
       render json: { input_fields: InputFieldSerializer.new(input_fields).serializable_hash, default_coverages: DefaultCoverageSerializer.new(default_coverages).serializable_hash }, status: :ok
     end
 
-    def create_company_bespoke_service
-      service_name = params[:name]
+    def create_company_bespoke_service      
+      service_name = params[:service_name]
       company_id = params[:company_id]
       base_service_id = params[:base_service_id]
-      other_service_ids = params[:other_service_ids] || []
-      unless service_name.present? && other_service_ids.present? && base_service_id.present? && company_id.present?
-        ## Add flash message
+      sub_category = params[:sub_category]
+      other_service_ids = params[:secondary_service_ids].select(&:present?) rescue []
+      unless service_name.present? && base_service_id.present? && company_id.present?
+        return render json: {error: "Please enter valid data"}, status: :unprocessable_entity
       end
       b_service = Service.find(base_service_id)
-      b_sub_cat = b_service.sub_categories.where("name ilike ?", "%full day%").first
-      b_sub_cat = b_service.sub_categories.first unless b_sub_cat.present?
+      b_sub_cat = b_service.sub_categories.where("name ilike ?", "%#{sub_category}%").first
+      unless b_sub_cat.present?
+        return render json: {error: "#{sub_category} sub category does not exist for selected base service"}, status: :unprocessable_entity
+      end
       b_cmp_sc = b_sub_cat.company_sub_categories.find_by(company_id: company_id)
-
       other_service_ids.delete(base_service_id)
       o_services = Service.where(id: other_service_ids)
       service = Service.new(name: service_name, description: b_service.description, company_id: company_id)
-      if service.save
+      if service.save!
         service.image.attach(b_service.image.blob) if b_service.image.attached?
         sub_cat = service.sub_categories.new(name: b_sub_cat.name, start_from: b_cmp_sc.price)
         if sub_cat.save
